ecaf6f3339a0cef25b2e0682961b7a4d68f7d6f8
Wojtbart <42579187+Wojtbart@users.noreply.github.com>
2026-06-24 22:06:36 +0200

    feat: configure Web project - Program.cs, DI registration, MudBlazor setup

--- PetWorld.Web/Program.cs
+++ PetWorld.Web/Program.cs
@@ -1,22 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using MudBlazor.Services;
+using PetWorld.Application.UseCases.GetHistory;
+using PetWorld.Domain.Interfaces;
+using PetWorld.Infrastructure.Agents;
+using PetWorld.Infrastructure.Persistence;
 using PetWorld.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddMudServices();
+
+var connectionString = builder.Configuration.GetConnectionString("Default")!;
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddScoped<IChatRepository, ChatRepository>();
+
+var apiKey = builder.Configuration["OpenAI:ApiKey"]!;
+var model = builder.Configuration["OpenAI:Model"] ?? "gpt-4o-mini";
+builder.Services.AddScoped<IAgentService>(_ => new AgentService(apiKey, model));
+
+builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssembly(typeof(GetHistoryHandler).Assembly));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
